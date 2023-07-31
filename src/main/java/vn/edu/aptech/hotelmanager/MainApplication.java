d3ee8553c03439237edaa43d969949d512854219
longchu <huonggiangnt@gmail.com>
2023-07-31 23:27:30 +0700

    them file moi

--- src/main/java/vn/edu/aptech/hotelmanager/MainApplication.java
+++ src/main/java/vn/edu/aptech/hotelmanager/MainApplication.java
@@ -12,35 +12,37 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import vn.edu.aptech.hotelmanager.controllers.MainController;
 import vn.edu.aptech.hotelmanager.controllers.SplashController;
+import vn.edu.aptech.hotelmanager.repo.db.DBConnection;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         CSSFX.start();
-//        FXMLLoader loader = new FXMLLoader(HMResourcesLoader.loadURL("fxml/Main.fxml"));
-//        loader.setControllerFactory(c -> new MainController(stage));
-//        Parent root = loader.load();
-//        Scene scene = new Scene(root);
-//        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
-//        scene.setFill(Color.TRANSPARENT);
-//        stage.initStyle(StageStyle.TRANSPARENT);
-//        stage.setScene(scene);
-//        stage.setTitle("Hotel FX");
-//        stage.show();
-        openSplash(stage);
+        FXMLLoader loader = new FXMLLoader(HMResourcesLoader.loadURL("fxml/Main.fxml"));
+        loader.setControllerFactory(c -> new MainController(stage));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setTitle("Hotel FX");
+        stage.show();
+ //       openSplash(stage);
     }
 
     public static void main(String[] args) {
         launch(args);
-//        try {
-//            DBConnection.getInstance().getConnection().close();
-//        } catch (SQLException throwable) {
-//            throwable.printStackTrace();
-//        }
+        try {
+            DBConnection.getInstance().getConnection().close();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
-    private static void openSplash(Stage stage) throws IOException {
+ /*   private static void openSplash(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(HMResourcesLoader.loadURL("fxml/Splash.fxml"));
         loader.setControllerFactory(c -> new SplashController(stage));
         Parent root = loader.load();
@@ -49,5 +51,5 @@ public class MainApplication extends Application {
         stage.setScene(scene);
         stage.setTitle("Splash");
         stage.show();
-    }
+    }*/
 }
