95d924e49db3104515d90f25220e0ffb2da780aa
longchu <huonggiangnt@gmail.com>
2023-08-09 18:26:47 +0700

    them

--- src/main/java/vn/edu/aptech/hotelmanager/controllers/RoomController.java
+++ src/main/java/vn/edu/aptech/hotelmanager/controllers/RoomController.java
@@ -23,7 +23,7 @@ import java.net.URL;
 import java.util.*;
 import java.util.List;
 
-public class RoomController implements Initializable {
+public class RoomController {
     private final IRoomRepo roomRepo = RepoFactory.getInstance().getRepo(REPO_TYPE.ROOM);
     private MFXGenericDialog dialogContent;
     private final Stage stage;
@@ -54,51 +54,20 @@ public class RoomController implements Initializable {
     @FXML
     private TableColumn<Room, Integer> status;
 
-    private List<Room> roomlist ;
-    private List<Room>  setRoomList()  throws Exception {
-        RoomDAOImpl roomDAO= new RoomDAOImpl();
-         return  roomlist = roomDAO.findAll();
-    }
 
 
     public RoomController(Stage stage){
         this.stage = stage;
     }
-    ObservableList<RoomEntity> roomEntityObservableList;
 
-    {
-        try {
-            roomEntityObservableList = FXCollections.observableList(
-                    setRoomList()
-            );
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void checkin(ActionEvent event) throws IOException {
         Stage window = new Stage();
         CheckinController checkinController = new CheckinController();
         CheckinController.display(event,window);
     }
-    public void occupied(ActionEvent event) throws IOException {
-        Stage window = new Stage();
-        CheckinController checkinController = new CheckinController();
-        CheckinController.display(event,window);
-    }
 
 
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        id.setCellValueFactory( new PropertyValueFactory<RoomEntity,Integer>("id"));
-        name.setCellValueFactory( new PropertyValueFactory<RoomEntity,String>("name"));
-        status.setCellValueFactory( new PropertyValueFactory<RoomEntity,Integer>("status"));
-        numberOfBeds.setCellValueFactory( new PropertyValueFactory<RoomEntity,Integer>("numberOfBeds"));
-        price.setCellValueFactory( new PropertyValueFactory<RoomEntity,Double>("price"));
-        category.setCellValueFactory( new PropertyValueFactory<RoomEntity,Integer>("category"));
-        flag.setCellValueFactory( new PropertyValueFactory<RoomEntity,Integer>("flag"));
-        rooms.setItems(roomEntityObservableList);
-    }
 }
